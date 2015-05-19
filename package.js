ecccdc1a374adccdb768a9122217f264d0ef7258
Andrew Youngwerth <drew.youngwerth@gmail.com>
2015-05-19 13:42:14 -0600

    Use momentjs for parsing
    Also sets default format to US

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'drewy:datetimepicker',
+  version: '1.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'jQuery Date Time Picker',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/drew-y/meteor-datetimepicker',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('jquery', 'momentjs:moment', 'client');
+
+  api.addFiles(['jquery.datetimepicker.js', 'jquery.datetimepicker.css'], 'client');
+});
