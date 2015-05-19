8f7da559a8932b45d4f8182ed3f4cb8485027b25
Andrew Youngwerth <drew.youngwerth@gmail.com>
2015-05-19 13:52:49 -0600

    Update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'drewy:datetimepicker',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'jQuery Date Time Picker',
   // URL to the Git repository containing the source code for this package.
